a83e5315efacc5cf7d309de4b399631c999f9ee4
Nick Hollon <falling.masonry@gmail.com>
2015-11-02 15:27:42 -0800

    Implement cubic spline interpolation

--- src/Interpolate/Cubic.elm
+++ src/Interpolate/Cubic.elm
@@ -1,3 +1,176 @@
-module Interpolate.Cubic where
+module Interpolate.Cubic (Spline, LocalCurve, withRange
+                         , valueAt, slopeAt, concavityAt, curveAt) where
 
-temp = identity
+import Array exposing (Array)
+
+
+valueAt : Float -> Spline -> Float
+valueAt =
+  evaluate cubic
+
+      
+slopeAt : Float -> Spline -> Float
+slopeAt =
+  evaluate firstDerivative
+
+           
+concavityAt : Float -> Spline -> Float
+concavityAt =
+  evaluate secondDerivative
+
+
+curveAt : Float -> Spline -> LocalCurve
+curveAt =
+  let
+    curve x coeff =
+      { value = cubic x coeff
+      , slope = firstDerivative x coeff
+      , concavity = secondDerivative x coeff
+      }
+  in
+    evaluate curve
+
+
+cubic : Float -> Coefficients -> Float
+cubic x { a, b, c, d } =
+  a*x^3 + b*x^2 + c*x + d
+
+
+firstDerivative : Float -> Coefficients -> Float
+firstDerivative x { a, b, c, d } =
+  3*a*x^2 + 2*b*x + c
+
+
+secondDerivative : Float -> Coefficients -> Float
+secondDerivative x { a, b, c, d } =
+  6*a*x + 2*b
+
+
+evaluate : (Float -> Coefficients -> a) -> Float -> Spline -> a
+evaluate f x (Spline spline) =
+  let
+    maxIndex =
+      Array.length spline.coefficients - 1
+           
+    index =
+      (x - spline.start) / spline.dx
+        |> floor
+        |> clamp 0 maxIndex
+
+    offset =
+      x - spline.dx * (toFloat index) - spline.start
+
+    coeff =
+      Array.get index spline.coefficients
+        |> Maybe.withDefault { a = 0, b = 0, c = 0, d = 0 }
+  in
+    f offset coeff
+
+      
+type alias LocalCurve =
+  { value : Float
+  , slope : Float
+  , concavity : Float
+  }
+
+                      
+withRange : Float -> Float -> List Float -> Maybe Spline
+withRange start end heights =
+  let
+    n =
+      List.length heights |> toFloat
+          
+    dx =
+      (end - start) / (n - 1)
+  in
+    if | n > 1 ->
+         { coefficients = findCoefficients dx heights
+         , start = start
+         , dx = dx
+         } |> Spline |> Just
+    
+       | otherwise ->
+         Nothing
+
+
+findCoefficients : Float -> List Float -> Array Coefficients
+findCoefficients dx heights =
+  let
+    concavity y0 y1 y2 =
+      6 * (y0 + y2 - 2 * y1) / dx^2
+
+    sweep y (uPrev, vPrev) =
+      (1/(4 - uPrev), (y - vPrev)/(4 - uPrev))
+
+    backSub (u, v) mNext =
+      v - u * mNext
+
+    piece (y0, m0) (y1, m1) =
+      { a = (m1 - m0) / 6 / dx
+      , b = m0 / 2
+      , c = (y1 - y0) / dx  -  (m1 + 2 * m0) * dx / 6
+      , d = y0
+      }
+  in
+    mapTriple concavity heights
+      |> List.scanl sweep (0, 0)
+      |> scanr backSub 0
+      |> List.map2 (,) heights
+      |> mapPair piece
+      |> Array.fromList
+
+
+mapTriple : (a -> a -> a -> b) -> List a -> List b
+mapTriple f x0 =
+  let
+    tail =
+      List.tail x0
+          
+    doubleTail =
+      Maybe.andThen tail List.tail
+  in
+    case (tail, doubleTail) of
+      (Just x1, Just x2) -> List.map3 f x0 x1 x2
+      otherwise -> []
+
+
+mapPair : (a -> a -> b) -> List a -> List b
+mapPair f x0 =
+  case (List.tail x0) of
+    Just x1 -> List.map2 f x0 x1
+    otherwise -> []
+
+
+scanr : (a -> b -> b) -> b -> List a -> List b
+scanr f init =
+  List.reverse >> List.scanl f init >> List.reverse
+                   
+
+type Spline =
+  Spline
+    { coefficients : Array Coefficients
+    , start : Float
+    , dx : Float
+    }
+            
+
+type alias Coefficients =
+  { a : Float
+  , b : Float
+  , c : Float
+  , d : Float
+  }
+            
+
+{- To implement:
+
+withDelta : Float -> Float -> List Float -> Spline
+
+slopeAt : Float -> Spline -> Float
+
+concavityAt : Float -> Spline -> Float
+
+curveAt : Float -> Spline -> { value : Float, slope : Float, concavity : Float }
+
+type Boundary = Natural | Parabolic | Cubic | Periodic | Clamped Float Float
+-}
--- test/Main.elm
+++ test/Main.elm
@@ -5,6 +5,7 @@
 
 module Main where
 
+import Debug
 import Text
 import Task exposing (Task)
 
@@ -16,13 +17,77 @@ import Console
 
 import Interpolate.Cubic as Cubic
 
-
 allTests : Test
 allTests =
-  suite "Test Suite"
-          [ test "First" (assertEqual 1 1)
+  suite "Cubic spline interpolation"
+          [ testLinear
+          , testCubic
           ]
 
+
+-- y = 1/2 x^3 - 3/2 x^2 + 1
+-- for x < 1, f(x) = y(1 - x)
+-- for 1 < x, f(x) = y(x - 1)
+testCubic : Test
+testCubic =
+  let
+    spline = Cubic.withRange 0 2 [0, 1, 0]
+  in
+    suite "three data points"
+            [ testCurve "x = 0.0" 0 spline
+                        { value = 0, slope = 1.5, concavity = 0 }
+                        
+            , testCurve "x = 0.5" 0.5 spline
+                        { value = 0.6875, slope = 1.125, concavity = -1.5 }
+                        
+            , testCurve "x = 1.0" 1 spline
+                        { value = 1, slope = 0, concavity = -3 }
+
+            , testCurve "x = 2.0" 2 spline
+                        { value = 0, slope = -1.5, concavity = 0 }
+
+            , testCurve "x = 3.0" 3 spline
+                        { value = -1, slope = 0, concavity = 3 }
+            ]
+
+
+-- tests that y = x + 1
+testLinear : Test
+testLinear =
+  let
+    testPoints name line =
+      suite name
+              [ testCurve "at x0" 0 line
+                            { value = 1, slope = 1, concavity = 0 }
+                            
+              , testCurve "at x1" 1 line
+                            { value = 2, slope = 1, concavity = 0 }
+                            
+              , testCurve "at midpoint" 0.5 line
+                            { value = 1.5, slope = 1, concavity = 0 }
+              ]
+  in
+    suite "two data points"
+            [ testPoints "data at 0 and 1" (Cubic.withRange 0 1 [1, 2])
+            , testPoints "data at 0 and 2" (Cubic.withRange 0 2 [1, 3])
+            , testPoints "data at -1 and 1" (Cubic.withRange -1 1 [0, 2])
+            ]
+                         
+    
+
+testCurve : String -> Float -> Maybe Cubic.Spline -> Cubic.LocalCurve -> Test
+testCurve name x mSpline expected =
+  case mSpline of
+    Just spline ->
+      suite name
+              [ test "value" (assertEqual expected.value (Cubic.valueAt x spline))
+              , test "slope" (assertEqual expected.slope (Cubic.slopeAt x spline))
+              , test "concavity" (assertEqual expected.concavity (Cubic.concavityAt x spline))
+              , test "local curve" (assertEqual expected (Cubic.curveAt x spline))
+              ]
+    Nothing ->
+      test "Bad Data!" (assertEqual 0 1)
+      
 
 port runner : Signal (Task a ())
 port runner =
